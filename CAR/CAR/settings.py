7a238fdbaa991294d872c42161604046f0cc179f
Waztom <waztom@gmail.com>
2021-02-25 15:00:59 +0000

    Rabbit added - yebo - yes - App Working

--- CAR/CAR/settings.py
+++ CAR/CAR/settings.py
@@ -143,9 +143,11 @@ LOGGING = {
 }
 
 # celery setup
-# CELERY_BROKER_URL = 'amqp://localhost'
-CELERY_BROKER_URL = "redis://redis:6379"
-CELERY_RESULT_BACKEND = 'redis://redis:6379'
-# CELERY_ACCEPT_CONTENT = ['application/json']
-# CELERY_RESULT_SERIALIZER = 'json'
-# CELERY_TASK_SERIALIZER = 'json'
+CELERY = {
+    'CELERY_BROKER_URL' : os.environ['CELERY_BROKER_URL'], # 'amqp://localhost' for local devlopment
+    'CELERY_RESULT_BACKEND': os.environ['CELERY_RESULT_BACKEND'], # 'redis://redis:6379'
+    'CELERY_ACCEPT_CONTENT': ['application/json'],
+    'CELERY_RESULT_SERIALIZER': 'json',
+    'CELERY_TASK_SERIALIZER': 'json',
+}
+
