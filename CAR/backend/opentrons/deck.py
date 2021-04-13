0fb72c4dfa5d2618e2fcb7503ce036ddabc54751
Olivia Simpson <52531289+OliviaSimpson@users.noreply.github.com>
2021-04-13 23:32:29 +0100

    added next free well & next free plate functions

--- CAR/backend/opentrons/deck.py
+++ CAR/backend/opentrons/deck.py
@@ -3,9 +3,23 @@ class Deck ():
     def __init__(self, index=None):
         self.deckindex = index
         self.PlateList = []
+
+    def __repr__(self):
+        return 'D{}'.format(self.deckindex)
+
+    def __str__(self):
+        return 'Deck {}'.format(self.deckindex)
     
     def nextfreeplate(self):
-        pass
+        freeplates = []
+        for plate in self.PlateList:
+            if plate.isempty():
+                freeplates.append(plate.plateindex)
+        if len(freeplates)>=1:
+            nextfreeplate= int(freeplates[0])
+        else:
+            nextfreeplate = "no free wells"
+        return nextfreeplate
 
     def addplate(self, location, numwells=None, platewellVolume=None):
         self.PlateList.append(Plate(self, index=len(self.PlateList), numwells=numwells,platewellVolume=platewellVolume))
@@ -18,26 +32,40 @@ class Plate ():
         self.deck = Deck
         self.plateindex = index
         self.numwells = numwells
-        self.platewellVolume = None
-        #self.WellList = None
+        self.platewellVolume = platewellVolume
+        self.WellList = None
         self.setupwells()
+
+    def __repr__(self):
+        return 'D{}P{}'.format(self.deck.deckindex, self.plateindex)
 
     def __str__(self):
         return 'Deck {}, Plate number {}'.format(self.deck.deckindex, self.plateindex)
 
     def setupwells(self):
-        self.setofwells = []
+        self.WellList = []
         for well in range(self.numwells):
-            self.setofwells.append(Well(self, wellindex=well, Volume=self.platewellVolume, VolumeUsed=0, StartSmiles="", GoalSmiles=""))
-        return self.setofwells
+            self.WellList.append(Well(self, wellindex=well, Volume=self.platewellVolume, VolumeUsed=0, StartSmiles="", GoalSmiles=""))
+        return self.WellList
 
 
     def nextfreewell(self):
-        for i in self.setofwells:
-            for j in i:
-                if self.setofwells[i,j].VolumeUsed == 0:
-                    print(str(i)+","+str(j))
-                    return [i,j]
+        freewells = []
+        for well in self.WellList:
+            if well.isempty():
+                freewells.append(well.wellindex)
+        if len(freewells)>=1:
+            nextfreewell = int(freewells[0])
+        else:
+            nextfreewell = "no free wells"
+        return nextfreewell
+
+    def isempty(self):
+        isempty = True
+        for well in self.WellList:
+            if not well.isempty():
+                isempty = False
+        return isempty
 
     def smilesearch(self, smiles, start_smiles=None, goal_smiles=None):
         if start_smiles == None & goal_smiles == None:
@@ -59,8 +87,8 @@ class Well ():
     def __init__(self, Plate, wellindex=None, Volume=None, VolumeUsed=0, StartSmiles=None, GoalSmiles=None):
         self.plate = Plate
         self.wellindex = wellindex
-        self.Volume = Volume
-        self.VolumeUsed = VolumeUsed
+        self.Volume = float(Volume)
+        self.VolumeUsed = float(VolumeUsed)
         self.StartSmiles = StartSmiles
         self.GoalSmiles = GoalSmiles
         
@@ -71,15 +99,23 @@ class Well ():
         return 'Deck {}, Plate {}, Well {}'.format(self.plate.deck.deckindex, self.plate.plateindex, self.wellindex)
     
     def add(self, Ammount):
-        SafetyMargin = 10
+        SafetyMargin = 5 # percentage of the well's volume to be keept empty to prevent overvlow
         WorkingVolumeused = self.VolumeUsed+Ammount
-        if WorkingVolumeused >= self.Volume*(1-(SafetyMargin/100)):
+        if WorkingVolumeused >= float(self.Volume)*(1-(SafetyMargin/100)):
             raise NameError("the resultant value of adding "+str(Ammount)+"ul  to "+str(self.VolumeUsed)+"ul would be "+str(WorkingVolumeused)+"ul exceeding the well's volume of "+str(self.Volume)+"ul (with a saftey margin of "+str(SafetyMargin)+"%)")
         elif WorkingVolumeused < 0:
-            raise NameError("the resultant value of adding "+str(Ammount)+"ul  to "+str(self.VolumeUsed)+"ul would be "+str(WorkingVolumeused)+"ul")
+            raise NameError("the resultant value of adding "+str(Ammount)+"ul to "+str(self.VolumeUsed)+"ul would be "+str(WorkingVolumeused)+"ul")
         else:
             self.VolumeUsed = WorkingVolumeused
         return self.VolumeUsed
+    
+    def isempty(self):
+        if self.VolumeUsed <= 0:
+            isempty = True
+        else:
+            isempty = False
+        return isempty
+
 
     def changesmiles(self, start_smiles=None, goal_smiles=None):
         if type(start_smiles) != None:
