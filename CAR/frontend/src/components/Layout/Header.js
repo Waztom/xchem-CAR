e4302b000764338b20c8e99c652a59370b8d8215
Waztom <waztom@gmail.com>
2021-07-10 23:01:42 +0000

    Added protocol gen dropdown/fixed delete in app

--- CAR/frontend/src/components/Layout/Header.js
+++ CAR/frontend/src/components/Layout/Header.js
@@ -10,23 +10,30 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Modal from "react-bootstrap/Modal";
 
 import { Trash } from "react-bootstrap-icons";
+import { HandThumbsUpFill } from "react-bootstrap-icons";
 
-function DeleteModal({ show, handleClose, handleDelete, id }) {
+function DeleteModal({
+  show,
+  handleDeleteClose,
+  handleDelete,
+  id,
+  projectName,
+}) {
   function handleConfirm() {
     handleDelete(id);
-    handleClose();
+    handleDeleteClose();
   }
 
   return (
-    <Modal show={show} onHide={handleClose} animation={false}>
+    <Modal show={show} onHide={handleDeleteClose} animation={false}>
       <Modal.Header closeButton>
         <Modal.Title>
           <Trash></Trash>Delete Project
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>Warning this will delete your project!</Modal.Body>
+      <Modal.Body>Warning this will delete project - {projectName}</Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={() => handleClose()}>
+        <Button variant="secondary" onClick={() => handleDeleteClose()}>
           Close
         </Button>
         <Button variant="warning" onClick={() => handleConfirm()}>
@@ -37,9 +44,46 @@ function DeleteModal({ show, handleClose, handleDelete, id }) {
   );
 }
 
-const Header = ({ changeProject, deleteProject }) => {
+function CreateModal({
+  show,
+  handleCreateClose,
+  handleCreate,
+  projectName,
+  id,
+}) {
+  function handleConfirm(id) {
+    handleCreateClose();
+    handleCreate(id);
+  }
+  return (
+    <Modal show={show} onHide={handleCreateClose} animation={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <HandThumbsUpFill></HandThumbsUpFill>Generate OT Protocol
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Would you like to create auto-generate protocols for project:{" "}
+        {projectName}?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => handleCreateClose()}>
+          Close
+        </Button>
+        <Button variant="warning" onClick={() => handleConfirm(id)}>
+          Create Protocol
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+const Header = ({ changeProject, deleteProject, generateProtocol }) => {
   const [Projects, setProjects] = useState([]);
-  const [show, setShow] = useState(false);
+  const [projectID, setProjectID] = useState([]);
+  const [projectName, setProjectName] = useState([]);
+  const [showDelete, setDeleteShow] = useState(false);
+  const [showCreate, setCreateShow] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -64,12 +108,27 @@ const Header = ({ changeProject, deleteProject }) => {
     deleteProject();
   }
 
+  function handleCreate(id) {
+    generateProtocol(id);
+  }
+
   function handleChange(projectid) {
     changeProject(projectid);
   }
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  function handleCreateShow(projectName) {
+    setCreateShow(true);
+    setProjectName(projectName);
+  }
+
+  function handleDeleteShow(projectName, id) {
+    setDeleteShow(true);
+    setProjectID(id);
+    setProjectName(projectName);
+  }
+
+  const handleDeleteClose = () => setDeleteShow(false);
+  const handleCreateClose = () => setCreateShow(false);
 
   return (
     <Navbar bg="light" expand="lg">
@@ -91,18 +150,40 @@ const Header = ({ changeProject, deleteProject }) => {
           <NavDropdown title="Delete project" id="collasible-nav-dropdown">
             {Projects.map((project) => (
               <React.Fragment>
-                <NavDropdown.Item key={project.id} onClick={() => handleShow()}>
+                <NavDropdown.Item
+                  key={project.id + 1}
+                  onClick={() => handleDeleteShow(project.name, project.id)}
+                >
                   {project.name}
                 </NavDropdown.Item>
-                <DeleteModal
-                  show={show}
-                  handleClose={handleClose}
-                  handleDelete={handleDelete}
-                  id={project.id}
-                ></DeleteModal>
               </React.Fragment>
             ))}
           </NavDropdown>
+          <DeleteModal
+            show={showDelete}
+            handleDeleteClose={handleDeleteClose}
+            handleDelete={handleDelete}
+            id={projectID}
+            projectName={projectName}
+          ></DeleteModal>
+          <NavDropdown title="Create OT Protocol" id="collasible-nav-dropdown">
+            {Projects.map((project) => (
+              <React.Fragment>
+                <NavDropdown.Item
+                  key={project.id + 2}
+                  onClick={() => handleCreateShow(project.name)}
+                >
+                  {project.name}
+                </NavDropdown.Item>
+              </React.Fragment>
+            ))}
+          </NavDropdown>
+          <CreateModal
+            show={showCreate}
+            handleCreateClose={handleCreateClose}
+            handleCreate={handleCreate}
+            projectName={projectName}
+          ></CreateModal>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
