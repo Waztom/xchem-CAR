95363d1a2d59923d747ed9cba6a8ed4c81ea6560
bluemoon-123 <harryrostron0@gmail.com>
2021-07-28 11:00:12 +0000

    Added Boc protect and sulfonamide recipe

--- CAR/backend/recipebuilder/encodedrecipes.py
+++ CAR/backend/recipebuilder/encodedrecipes.py
@@ -225,6 +225,58 @@ encoded_recipes = {
             },
         ],
     },
+    "sulfonamide schotten-baumann": {
+        "reactionSMARTS": ["[#6:1]-[#17:3].[#7:8]>>[#6:1]-[#7:8]"],
+        "recipe": [
+            {
+                "name": "add",
+                "content":{
+                    "action_no": 1,
+                    "material":{
+                        "SMARTS":["[#6:1](=[#8])-[#17:3]"],
+                        "SMILES": None,
+                        "quantity": {"value": 1, "unit": "moleq"},
+                        "solvent": "MeOH", #solvent for all can be IPA, EtOAc or MeOH will need to test
+                        "concentration": 0.5,
+                    },
+                },
+            },
+            {
+                "name": "add",
+                "content":{
+                    "action_no": 2,
+                    "material":{
+                        "SMARTS":["[#7:8]-[#16:5](=[#8:6])(=[#8:7])-[#6:4]"],
+                        "SMILES": None,
+                        "quanity": {"value": 1.2, "unit": "moleq"},
+                        "solvent": "MeOH",
+                        "concentration": 0.5,
+                    },
+                },
+            },
+            {
+                "name": "add",
+                "content":{
+                    "action_no": 3, # using DBU as pKa of sulfonamide ~10, DBU pKa ~13
+                    "material":{
+                        "SMARTS": None,
+                        "SMILES": "C1CCC2=NCCCN2CC1",
+                        "quantity": {"value": 3.5, "unit": "moleq"},
+                        "solvent": "MeOH",
+                        "concentration": 0.5,
+                    },
+                },
+            },
+            {
+                "name": "stir",
+                "content":{
+                    "action_no": 4,
+                    "temperature": {"value": 100, "unit": "degC"},  
+                    "duration": {"value": 12, "unit": "hours"}, 
+                },
+            },
+        ],
+    },
     "sp2-sp2 suzuki coupling": {
         "reactionSMARTS": ["[c:1]-[F,Cl,Br,I].[#6:2]-[B]>>[c:1]-[#6:2]"],
         "recipe": [
@@ -291,8 +343,98 @@ encoded_recipes = {
             },
         ],
     },
+    "Boc protection": {
+        "reactionSMARTS": ["[#7:2].[#8:3]-[#6:1](=[#8:4])-[#8:5]>>[#7:2]-[#6:1](=[#8])-[#8]-[#6](-[#6])(-[#6])(-[#6])"],
+        "recipe": [
+            {
+                "name": "add",
+                "content":{
+                    "action_no": 1,
+                    "material":{
+                        "SMARTS": ["[#8:3]-[#6:1](=[#8:4])-[#8:5]-[#6](-[#6])(-[#6])(-[#6])"],#BOC
+                        "SMILES": None,
+                        "quantity": {"value": 1.5, "unit": "moleq"},
+                        "solvent": "ACN",
+                        "concentration": 0.5,
+                    },
+                },
+            },
+            {
+                "name": "add",
+                "content":{
+                    "action_no": 2,
+                    "material":{
+                        "SMARTS": None,
+                        "SMILES": "n1ccc(N(C)C)cc1",
+                        "quantity": {"value": 1, "unit": "moleq"},#DMAP
+                        "solvent": "ACN",
+                        "concentration": 0.5,
+                    },
+                },
+            },
+            {   
+                "name": "add",
+                "content":{
+                    "action_no": 3,
+                    "material":{
+                        "SMARTS": ["[#7;H2]"],
+                        "SMILES": None,
+                        "quantity": {"value": 1, "unit": "moleq"},#primary amine
+                        "solvent": "ACN",
+                        "concentration": 0.5,
+                    },
+                },
+            },
+            {
+                 "name": "stir",
+                 "content":{
+                    "action_no": 4,
+                    "temperature": {"value": 100, "unit": "degC"},  
+                    "duration": {"value": 12, "unit": "hours"},
+                },
+            },
+        ],
+    },
+    "Boc deprotection": {#this does not work yet, SMARTS needs rethinking
+        "reactionSMARTS":["[#7:2]-[#6:1](=[#8])-[#8]-[#6](-[#6])(-[#6])(-[#6])]>>[#7:2].[#6:1](=[#8])-[#8]-[#6](-[#6])(-[#6])(-[#6])]"],
+        "recipe":[
+            {
+                "name": "add",
+                "content":{
+                    "action_no": 1,
+                    "material":{
+                        "SMARTS": ["[#7:2]-[#6:1](=[#8])-[#8]-[#6](-[#6])(-[#6])(-[#6])]"],
+                        "SMILES": None,
+                        "quantity": {"value": 1, "unit": "moleq"},
+                        "solvent": "EtOAc",
+                        "concentration": 0.5,
+                    },
+                },
+            },
+            {
+                "name": "add",
+                "content":{
+                    "action_no": 2,
+                    "material":{
+                        "SMARTS": None,
+                        "SMILES": "HCl",
+                        "quantity": {"value": 1, "unit": "moleq"},
+                        "solvent": "EtOAc",
+                        "concentration": 6
+                    },
+                },
+            },
+            {
+                "name": "stir",
+                 "content":{
+                    "action_no": 3,
+                    "temperature": {"value": 25, "unit": "degC"},  
+                    "duration": {"value": 12, "unit": "hours"},
+                },
+            },
+        ],
+    },
 }
-
 
 
                 
