9d07d2f1325157858c9e27f9fbe5384608c34d02
Anton Gasperak <anton.gasperak@m2ms.sk>
2022-01-27 12:19:49 +0000

    Fix (temporarily) RDKit

--- CAR/frontend/src/RDKit/RDKitFunctions.js
+++ CAR/frontend/src/RDKit/RDKitFunctions.js
@@ -1,13 +1,6 @@
-var Module = {
-  onRuntimeInitialized: initRDKitModule().then(function (instance) {
-    Module = instance;
-    console.log("version: " + Module.version());
-  }),
-};
-
-function checkmol(text) {
+export function checkmol(text) {
   if (text.length > 0) {
-    var mol = Module.get_mol(text);
+    var mol = window.RDKit.get_mol(text);
     if (mol.is_valid()) {
       return mol;
     } else {
@@ -18,12 +11,12 @@ function checkmol(text) {
   }
 }
 
-function getsvg(mol) {
+export function getsvg(mol) {
   var svg = mol.get_svg();
   return svg;
 }
 
-function comparesmiles(mol1, mol2) {
+export function comparesmiles(mol1, mol2) {
   var smiles1 = mol1.get_smiles();
   var smiles2 = mol2.get_smiles();
 
