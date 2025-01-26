c0a20d6e801ae73f6e9acdf764686505cc1aea7e
Waztom <waztom@gmail.com>
2025-01-26 20:44:11 +0000

    Converted seevral .js into jsx to Vite compile

--- CAR/frontend/vite.config.js
+++ CAR/frontend/vite.config.js
@@ -2,33 +2,51 @@ import { defineConfig, transformWithEsbuild } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   // plugins: [react()],
+//   plugins: [
+//     {
+//       name: 'treat-js-files-as-jsx',
+//       async transform(code, id) {
+//         if (!id.match(/src\/.*\.js$/))  return null
+
+//         // Use the exposed transform from vite, instead of directly
+//         // transforming with esbuild
+//         return transformWithEsbuild(code, id, {
+//           loader: 'jsx',
+//           jsx: 'automatic',
+//         })
+//       },
+//     },
+//     react(),
+//   ],
+
+//   optimizeDeps: {
+//     force: true,
+//     esbuildOptions: {
+//       loader: {
+//         '.js': 'jsx',
+//       },
+//     },
+//   },
+//   server: {
+//     watch: { usePolling: true },
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//       },
+//       '/upload': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react()],
-  plugins: [
-    {
-      name: 'treat-js-files-as-jsx',
-      async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/))  return null
-
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
-        return transformWithEsbuild(code, id, {
-          loader: 'jsx',
-          jsx: 'automatic',
-        })
-      },
-    },
-    react(),
-  ],
-
-  optimizeDeps: {
-    force: true,
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
+  plugins: [react()],
   server: {
     watch: { usePolling: true },
     proxy: {
