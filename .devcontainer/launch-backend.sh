18e7ca514a727899f7df1fb0d8a80a6ebffca985
Waztom <waztom@gmail.com>
2025-02-17 20:31:52 +0000

    Added backend - to be tested

---
+++ .devcontainer/launch-backend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running migrations..."
+cd /code
+# initial migrations for existing stack
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# echo "Running collectstatic..."
+# python3 manage.py collectstatic --noinput -v 0 # collect static files
